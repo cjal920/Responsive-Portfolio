32cd0473dc7612785f206d786e7fc078faf83f2f
Colin Larkin <colinlarkin@Colins-MacBook-Air.local>
2018-05-24 16:22:12 -0400

    updating code

---
+++ assets/js/logic.js
@@ -0,0 +1,16 @@
+document.getElementById("about").onclick = function () {
+    location.href = "index.html";
+};
+
+document.getElementById("portfolio").onclick = function () {
+    location.href = "portfolio.html";
+};
+
+document.getElementById("contact").onclick = function () {
+    location.href = "contact.html";
+};
+
+document.getElementById("submit").onclick = function () {
+    location.href = "index.html";
+};
+
